827b728062522846c7f955ce95111016296ae40a
Albin Brolin <crystallizedtester@gmail.com>
2022-04-20 21:54:33 +0200

    Added names array randomzier

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 
 var names = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran♀", "Nidorina", "Nidoqueen", "Nidoran♂", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetch’d", "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie", "Mr. Mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo", "Mew"];
 
+//en array med alla Pokémons namn från Kanto regionen
 
-console.log(names[0])
 
 temp()
 
 function temp() {
     let i = Math.floor(Math.random() * names.length);
 
+    // Math.random() ger ett värde mellan 0 och 1      
+    // names.length är längden på arrayn (151 namn)
+    // Math.random() * names.length) =  ett tal mellan 0 och 151 (0.3, 4, 25, 76.9, osv)
+    // Math.floor avrundar nedåt till närmate heltal så det kan inte bli någon decimal
+    /// Allt det här betyder alltså att vi slumpar värdet på [i] varje gång
+
     document.querySelector("h1").textContent = names[i];
+
+    // document.querySelector() returnerar det första elementet inom dokumentet som matchar den specifika "väljaren"
+    /// I det här fallet retuneras "h1" 
+    /// som varje gång är i:ondes namn i names arrayn (helt slumpat förståss)
+
 }
+
+// Resultatet blir att webbsidan varje gång på refresh visar ett nytt namn (kan vara samma) då Math.random() varje gång får ett nytt svar 
+
